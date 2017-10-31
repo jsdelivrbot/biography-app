68bf1945a07518b63dc030ef9d769cfc887b2f92
Vaibhao <vaibhavdhage14@gmail.com>
2017-10-30 18:27:23 -0700

    first commit

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+  {title: 'Harry Potter', pages: 405},
+  {title: 'Iron Man', pages:101},
+  {title: 'Batman', pages:120},
+  {title: 'Spiderman', pages:210}
+];
+}
